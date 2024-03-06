bb4bdb446ac0d9ba80b6c2c15531ca746f7d0911
Pavcho <pavkokal@gmail.com>
2024-03-06 19:57:22 +0200

    added different frames in main
    added different frames for the main menu, the figures and the board

--- chess_project/main.py
+++ chess_project/main.py
@@ -6,9 +6,30 @@ customtkinter.set_default_color_theme("dark-blue")
 
 
 # creating the window
+width = 900
+height = 600
 root = customtkinter.CTk()
-root.geometry("900x600")
+root.geometry(f"{width}x{height}")
 root.title("Chess game")
+
+
+frame_menu = customtkinter.CTkFrame(master=root,
+                                    width=width,
+                                    height=height
+                                    )
+frame_menu.grid(row=0, column=0)
+
+frame_figures = customtkinter.CTkFrame(master=root,
+                                       width=width,
+                                       height=height
+                                       )
+frame_figures.grid(row=0, column=0)
+
+frame_board = customtkinter.CTkFrame(master=root,
+                                     width=width,
+                                     height=height
+                                     )
+frame_board.grid(row=0, column=0)
 
 
 # functions for the buttons
@@ -16,62 +37,89 @@ def play():
     print("Play")
 
 
-def change_figures():
-    print("Change figures")
-
-
 def change_board():
-    print("Change board")
+    frame_board.tkraise()
+
+    button5_figures = customtkinter.CTkButton(
+        frame_board,
+        text="Back to menu",
+        corner_radius=8,
+        height=45,
+        width=200,
+        font=("Cosmic Sans", 23),
+        command=create_menu_frame
+    )
+    button5_figures.place(x=20, y=20)
 
 
 # start game button
-button1 = customtkinter.CTkButton(
-    root,
-    text="Play",
-    corner_radius=8,
-    height=45,
-    width=250,
-    font=("Cosmic Sans", 23),
-    command=play
-)
-button1.place(x=320, y=100)
+def create_menu_frame():
+    frame_menu.tkraise()
 
-# change figures button
-button2 = customtkinter.CTkButton(
-    root,
-    text="Figures",
-    corner_radius=8,
-    height=45,
-    width=250,
-    font=("Cosmic Sans", 23),
-    command=change_figures
-)
-button2.place(x=320, y=200)
+    button1_menu = customtkinter.CTkButton(
+        frame_menu,
+        text="Play",
+        corner_radius=8,
+        height=45,
+        width=250,
+        font=("Cosmic Sans", 23),
+        command=play
+    )
+    button1_menu.place(x=320, y=100)
 
-# change board button
-button3 = customtkinter.CTkButton(
-    root,
-    text="Board",
-    corner_radius=8,
-    height=45,
-    width=250,
-    font=("Cosmic Sans", 23),
-    command=change_board
-)
-button3.place(x=320, y=300)
+    # change figures button
+    button2_menu = customtkinter.CTkButton(
+        frame_menu,
+        text="Figures",
+        corner_radius=8,
+        height=45,
+        width=250,
+        font=("Cosmic Sans", 23),
+        command=create_figures_frame
+    )
+    button2_menu.place(x=320, y=200)
 
-# exit button
-button4 = customtkinter.CTkButton(
-    root,
-    text="Exit",
-    corner_radius=8,
-    height=45,
-    width=250,
-    font=("Aerial", 23),
-    command=root.quit
-)
-button4.place(x=320, y=400)
+    # change board button
+    button3_menu = customtkinter.CTkButton(
+        frame_menu,
+        text="Board",
+        corner_radius=8,
+        height=45,
+        width=250,
+        font=("Cosmic Sans", 23),
+        command=change_board
+    )
+    button3_menu.place(x=320, y=300)
 
+    # exit button
+    button4_menu = customtkinter.CTkButton(
+        frame_menu,
+        text="Exit",
+        corner_radius=8,
+        height=45,
+        width=250,
+        font=("Aerial", 23),
+        command=root.quit
+    )
+    button4_menu.place(x=320, y=400)
+
+
+def create_figures_frame():
+    frame_figures.tkraise()
+
+    button5_figures = customtkinter.CTkButton(
+        frame_figures,
+        text="Back to menu",
+        corner_radius=8,
+        height=45,
+        width=200,
+        font=("Cosmic Sans", 23),
+        command=create_menu_frame
+    )
+    button5_figures.place(x=20, y=20)
+
+
+create_menu_frame()
 
 # mainloop
 root.mainloop()
